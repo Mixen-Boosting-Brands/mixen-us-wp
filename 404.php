7b13c1b1beb0dbd497cba8694aa2758d27a3090e
Luis Carlos Pando <hey@luiscarlospando.com>
2025-06-30 10:14:45 -0600

    Build 404 view

--- 404.php
+++ 404.php
@@ -1,24 +1,34 @@
-<?php get_header(); ?>
+<?php
+get_header();
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+// Set a fallback image URL for the 404 page jumbotron
+$placeholder_url = get_template_directory_uri() . "/assets/images/404/404.jpg";
+?>
 
-			<!-- article -->
-			<article id="post-404">
+<section id="jumbotron" class="parallax-window" data-parallax="scroll" data-image-src="<?php echo esc_url(
+    $placeholder_url
+); ?>">
+  <div id="overlay"></div>
+</section>
 
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section id="body" class="py-60">
+  <div class="container">
+    <div class="row mb-1 mb-lg-3">
+      <div class="col-lg-8 offset-lg-2">
+        <h1 class="titulo-page">
+          <span class="fs-4">Oops — Page Not Found</span>
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+        <p>Sorry, the page you’re looking for doesn’t exist or has been moved.</p>
+        <a href="<?php echo esc_url(
+            home_url()
+        ); ?>" class="btn btn-primary mt-3">Go to Homepage</a>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
