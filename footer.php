36591e45a344309cb732ecd41ac747c19563166e
Luis Carlos Pando <hey@luiscarlospando.com>
2025-07-21 09:11:05 -0600

    Install additional Google Analytics code

--- footer.php
+++ footer.php
@@ -341,5 +341,15 @@
 
           gtag('config', 'G-62TM6X70DJ');
         </script>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17354272914">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17354272914');
+        </script>
     </body>
 </html>
